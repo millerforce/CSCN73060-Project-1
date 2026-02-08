79cdfe439613e41f0d56490ce145596b9a7bd8df
Connor Miller <125515698+millerforce@users.noreply.github.com>
2026-02-08 17:29:29 -0500

    moved type files and updated them to match server schema

--- web/src/http/types/post.ts
+++ web/src/http/types/post.ts
@@ -9,6 +9,16 @@ export type Post = {
     author: Account;
     content: string;
     numberOfLikes: number;
+    numberOfComments: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export type Comment = {
+    id: number;
+    postId: number;
+    account: Account;
+    content: string;
     createdAt: Date;
     updatedAt: Date;
 }
