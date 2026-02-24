ddcd833dd514bc1e8214f727a2beb97eeedf87c5
P-William <w.paetz@uzwelo.com>
2026-02-24 18:46:33 -0500

    attempted session interceptor fix

--- froggy-app/src/main/java/com/group1/froggy/app/auth/SessionInterceptor.java
+++ froggy-app/src/main/java/com/group1/froggy/app/auth/SessionInterceptor.java
@@ -4,6 +4,7 @@ import com.group1.froggy.app.controllers.AuthorizationController;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
+import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -15,8 +16,8 @@ public class SessionInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) throws IOException {
         if (handler instanceof HandlerMethod hm) {
-            boolean required = hm.getMethodAnnotation(RequireSession.class) != null
-                || hm.getBeanType().getAnnotation(RequireSession.class) != null;
+            boolean required = hm.hasMethodAnnotation(RequireSession.class)
+                || AnnotatedElementUtils.hasAnnotation(hm.getBeanType(), RequireSession.class);
             if (required) {
                 String cookie = request.getHeader(AuthorizationController.COOKIE_HEADER);
                 if (cookie == null || cookie.trim().isEmpty()) {
